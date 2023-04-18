90199e5050747b5c250630d17c258b3b066270ba
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2023-04-17 21:28:58 -0600

    se logró agregar botones al scroll bar, todavía falta poder configuar cada uno de ellos para que muestre la información respectiva

--- ventanasEtapa2/handle_requests.cpp
+++ ventanasEtapa2/handle_requests.cpp
@@ -1,11 +1,37 @@
 #include "handle_requests.h"
 #include "ui_handle_requests.h"
+#include <QVBoxLayout>
+#include <QPushButton>
 
 handle_requests::handle_requests(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::handle_requests)
 {
     ui->setupUi(this);
+    ui->scrollArea->setVerticalScrollBar(ui->verticalScrollBar);
+    auto * container = new QWidget();
+    ui->scrollArea->setWidget( container );
+    auto * layout = new QVBoxLayout(container);
+    auto * button1 = new QPushButton( "1", container);
+    auto * button2 = new QPushButton( "2", container);
+    auto * button3 = new QPushButton( "3", container);
+    auto * button4 = new QPushButton( "4", container);
+    auto * button5 = new QPushButton( "5", container);
+    auto * button6 = new QPushButton( "6", container);
+    auto * button7 = new QPushButton( "7", container);
+    auto * button8 = new QPushButton( "8", container);
+    auto * button9 = new QPushButton( "9", container);
+    layout->addWidget( button1 );
+    layout->addWidget( button2 );
+    layout->addWidget( button3 );
+    layout->addWidget( button4 );
+    layout->addWidget( button5 );
+    layout->addWidget( button6 );
+    layout->addWidget( button7 );
+    layout->addWidget( button8 );
+    layout->addWidget( button9 );
+
+
 }
 
 handle_requests::~handle_requests()
