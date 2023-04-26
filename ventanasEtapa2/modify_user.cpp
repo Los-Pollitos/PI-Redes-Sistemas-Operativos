cf411c56ab5224a5efed6b9a72ead286167ab3bb
angies23 <angie.solismanzano@ucr.ac.cr>
2023-04-25 21:00:20 -0600

    modificar usuario despliga la informacion y la permite modificar

--- ventanasEtapa2/modify_user.cpp
+++ ventanasEtapa2/modify_user.cpp
@@ -1,11 +1,11 @@
 #include "modify_user.h"
 #include "ui_modify_user.h"
 
+#include <QMessageBox>
+
 modify_user::modify_user(QWidget *parent) : QDialog(parent), ui(new Ui::modify_user) {
     ui->setupUi(this);
-    ui->comboBox->addItem("Empleado 1");
-    ui->comboBox->addItem("Empleado 2");
-    ui->comboBox->addItem("Empleado 3");
+    this->add_data();
 }
 
 modify_user::~modify_user() {
@@ -14,17 +14,46 @@ modify_user::~modify_user() {
 
 void modify_user::on_comboBox_activated(int index) {
     // TODO(nosotros): rellenar info de usuarios con cantidad de usuarios real
-    int total_users = 3;
-    if (index == 0) {
-        ui->employee_name->setText("Nombre empleado 1");
+    ui->employee_name->setText(this->names[index]);
+    ui->employee_id->setReadOnly(true);
+    ui->employee_id->setText(QString::number(this->ids[index]));
+    ui->employee_job->setText(this->job[index]);
+    ui->employee_subordinate->setText(this->subordinate[index]);
+    ui->employee_salary->setText(QString::number(this->salary[index]));
+    ui->employee_vacations->setText(QString::number(this->vacations[index]));
+}
+
+void modify_user::add_data() {
+    // TODO(nosotros): leer de una base de datos
+    ui->comboBox->addItem("Empleado 1");
+    ui->comboBox->addItem("Empleado 2");
+
+    this->names.append("Nombre1 Apellido1");
+    this->names.append("Nombre2 Apellido2");
+
+    this->ids.append(12345678);
+    this->ids.append(87654321);
+
+    this->job.append("Trabajo1");
+    this->job.append("Trabajo2");
+
+    this->subordinate.append("Yo1");
+    this->subordinate.append("Yo2");
+
+    this->salary.append(123);
+    this->salary.append(321);
+
+    this->vacations.append(1);
+    this->vacations.append(2);
+}
+
+void modify_user::on_approve_changes_clicked() {
+    QString password = ui->password->text();
+    if (password == "123") { // TODO(nosotros): password
+        QMessageBox::information(this, "Correcto", "Cambios aplicados");
+        this->close();
     } else {
-        if (index == 1) {
-            ui->employee_name->setText("Nombre empleado 2");
-        }
-        else {
-            if (index == 2) {
-                ui->employee_name->setText("Nombre empleado 3");
-            }
-        }
+        QMessageBox::warning(this, "Error", "Contraseña incorrecta");
     }
 }
+
--- ventanasEtapa2/modify_user.h
+++ ventanasEtapa2/modify_user.h
@@ -17,9 +17,19 @@ public:
 
 private slots:
     void on_comboBox_activated(int index);
+    void add_data();
+
+    void on_approve_changes_clicked();
 
 private:
     Ui::modify_user *ui;
+    QList<QString> names;
+    QList<int> ids;
+    QList<QString> job;
+    QList<QString> subordinate;
+    QList<int> salary;
+    QList<int> vacations;
+    // TODO(Angie): expediente
 };
 
 #endif // MODIFY_USER_H
