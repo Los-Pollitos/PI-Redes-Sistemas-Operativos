7704e643de6065d31a7fe1b25527d34e646e2af8
Cristopher Hernandez <cristo1206hc@gmail.com>
2023-05-09 20:46:41 -0600

    El cambio de contraseña es implementado.

--- Interfaz/change_password.cpp
+++ Interfaz/change_password.cpp
@@ -1,6 +1,11 @@
 #include "change_password.h"
 #include "ui_change_password.h"
 
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <QMessageBox>
+
 change_password::change_password(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::change_password)
@@ -9,9 +14,87 @@ change_password::change_password(QWidget *parent) :
     this->ui->lineEdit_2->setEchoMode(QLineEdit::Password); // no muestra password
     this->ui->lineEdit_3->setEchoMode(QLineEdit::Password); // no muestra token
     this->ui->lineEdit_4->setEchoMode(QLineEdit::Password); // no muestra confirmacion
+    this->pos = (int)(rand()%6);
+    int columna = pos%3 +1;
+    int fila = pos/3+1;
+    this->ui->indicacion_label->setText("Fila: " + QString::number(fila) + " Columna: " + QString::number(columna));
 }
 
 change_password::~change_password()
 {
     delete ui;
 }
+
+bool change_password::change_data(QString username, QString password) {
+    bool success = false;
+    //   try {
+    std::ifstream file("../Etapa 2/Archivos/Login.txt");
+    std::string read_data = "";
+    std::string buffer = "";
+    if (file.is_open()) {
+        while (file) {
+            file >> read_data;
+            if (read_data != username.toStdString()) {
+                buffer += read_data;
+                file >> read_data;
+                buffer += " " + read_data;
+                for (int i = 0; i < TOKEN_SIZE; ++i) {
+                    file >> read_data;
+                    buffer += " " + read_data;
+                }
+                read_data = "";
+            } else {
+                std::cout << read_data << std::endl;
+                buffer += read_data;
+                file >> read_data;
+                buffer += " " + password.toStdString();
+                for (int i = 0; i < TOKEN_SIZE; ++i) {
+                    file >> read_data;
+                    this->token[i] = std::stoi(read_data);
+                    buffer += " " + read_data;
+                    std::cout << read_data << std::endl;
+                }
+                read_data = "";
+                if (std::stoi(this->ui->lineEdit_3->text().toStdString()) == this->token[pos])
+                    success = true;
+            }
+            buffer += "\n";
+        }
+        file.close();
+
+        std::ofstream ofs("../Etapa 2/Archivos/Login.txt");
+
+        if (!ofs.is_open())
+        {
+            std::cerr << "Error cambiando contraseña" << std::endl;
+        }
+        ofs << buffer;
+        ofs.close();
+    } else {
+        std::cerr << "Archivo de login no encontrado" << std::endl;
+    }
+    //   } catch (_exception e) {  // TODO (nosotros): volver después
+    //       std::cerr << "Archivo de login no encontrado" << std::endl;
+    //   }
+    return success;
+}
+
+
+
+void change_password::on_pushButton_reinicarContra_clicked()
+{
+    if (this->ui->lineEdit_2->text() != this->ui->lineEdit_4->text()){
+        QMessageBox::warning(this, "Error", "Las contraseñas no son iguales");
+    } else {
+        if (this->change_data(this->ui->lineEdit->text(), this->ui->lineEdit_2->text())) {
+            this->hide();
+        } else {
+            QMessageBox::warning(this, "Error", "Datos incorrectos");
+            this->ui->lineEdit->setText("");
+            this->ui->lineEdit_2->setText("");
+            this->ui->lineEdit_3->setText("");
+            this->ui->lineEdit_4->setText("");
+        }
+    }
+}
+
--- Interfaz/change_password.h
+++ Interfaz/change_password.h
@@ -1,6 +1,8 @@
 #ifndef CHANGE_PASSWORD_H
 #define CHANGE_PASSWORD_H
 
+#include "login_info.h"
+
 #include <QDialog>
 
 namespace Ui {
@@ -14,9 +16,15 @@ class change_password : public QDialog
 public:
     explicit change_password(QWidget *parent = nullptr);
     ~change_password();
+    bool change_data(QString username, QString password);
+
+private slots:
+    void on_pushButton_reinicarContra_clicked();
 
 private:
     Ui::change_password *ui;
+    int pos;
+    int token[TOKEN_SIZE];
 };
 
 #endif // CHANGE_PASSWORD_H
