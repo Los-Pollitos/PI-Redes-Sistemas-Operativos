1ed20f37886cb2927ed371ca4cad9aa253272e87
GoninDS <luisdi2509@gmail.com>
2023-06-07 13:18:31 -0600

    Limpia de clase manage user

--- Interfaz/manage_user.h
+++ Interfaz/manage_user.h
@@ -2,8 +2,6 @@
 #define MANAGE_USER_H
 
 #include "login_info.h"
-
-#include "file_system.h"
 #include "client.h"
 
 #include <QDialog>
@@ -18,24 +16,9 @@ class manage_user : public QDialog
 
 public:
     explicit manage_user(QWidget *parent = nullptr);
-    void set_user_login(login_info * user_login);
-    void set_file_system(FS* file_system);
-    void set_client(client* local_client);
     ~manage_user();
-
-private:
-    bool find_user(std::string& desired_username);
-    // Uses file system object
-    void insert_user_file_system(std::string& desired_username, std::string& desired_password);
-    void delete_user_file_system(std::string& desired_username);
-    // Uses fstream
-    void insert_user_login(std::string& desired_username, std::string& desired_password);
-    void insert_user_data(std::string& desired_username, std::string& name, std::string& id);
-    void clean_aux_file();
-    void reinsert_login_file();
-    void reinsert_data_file();
-    void delete_user_login(std::string& desired_username);
-    void delete_user_data(std::string& desired_username);
+    void set_user_login(login_info * user_login);
+    void set_client(client* local_client);
 
 private slots:
     void on_generate_button_clicked();
@@ -44,7 +27,6 @@ private slots:
 private:
     Ui::manage_user* ui;
     login_info* user_login;
-    FS* file_system;
     client* local_client;
 };
 
