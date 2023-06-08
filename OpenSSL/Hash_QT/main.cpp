7b34c7ff56b81213acc82d9b07ba025b63ecdc15
GoninDS <luisdi2509@gmail.com>
2023-06-07 21:01:20 -0600

    Contraseñas con hash actualizadas en Login.txt

--- OpenSSL/Hash_QT/main.cpp
+++ OpenSSL/Hash_QT/main.cpp
@@ -6,8 +6,8 @@
 
 #include <iostream>
 
-std::string hash(QString password) {
-    QString answer = QString(QCryptographicHash::hash((password.toStdString()),QCryptographicHash::Md5).toHex());
+std::string hash_string(std::string password) {
+    QString answer = QString(QCryptographicHash::hash((password),QCryptographicHash::Md5).toHex());
     return answer.toStdString();
 }
 
@@ -15,7 +15,7 @@ std::string hash(QString password) {
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
-    std::cout << hash("luis") << "\n";
+    std::cout << "cjimenez :"<< hash_string("(e!zR$Y6/8TSgRs$") << "\n\n";	std::cout << "gmora :"<< hash_string("d{QbnP8(}kp") << "\n\n";	std::cout << "jnuñez :"<< hash_string("Ly=)Z6P4eUE") << "\n\n";	std::cout << "eguzman :"<< hash_string("3526283_eg@1998") << "\n\n";	std::cout << "alopez :"<< hash_string("&trabajoFeliz34!") << "\n\n";	std::cout << "respinoza :"<< hash_string("bRc=Q+->6$") << "\n\n";	std::cout << "gherrera :"<< hash_string("contrasena?DeAs1stenteMuySegura") << "\n\n";	std::cout << "rgang :"<< hash_string("r1cardo@gAng_?}") << "\n\n";	std::cout << "dcabezas :"<< hash_string("kfc&pollo_las_24horas!") << "\n\n";	std::cout << "d_gmora :"<< hash_string("3526283_e@g1998") << "\n\n";	return 0;
 
     return 0;
 }
