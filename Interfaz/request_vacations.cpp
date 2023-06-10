a47a0f46f403747c3ca63f4c70b84e3578028cc7
GoninDS <luisdi2509@gmail.com>
2023-06-10 17:11:53 -0600

    Diseño inicial de request vacations

--- Interfaz/request_vacations.cpp
+++ Interfaz/request_vacations.cpp
@@ -10,8 +10,6 @@ request_vacations::request_vacations(QWidget *parent) :
     ui->setupUi(this);
     this->setStyleSheet("background-color: #ECEAE5;");
     this->ui->confirm->setStyleSheet("color: #001f21;");
-    this->ui->end_date->setStyleSheet("color: #001f21;");
-    this->ui->label_end->setStyleSheet("color: #001f21;");
     this->ui->label_start->setStyleSheet("color: #001f21;");
     this->ui->reset->setStyleSheet("color: #001f21;");
     this->ui->start_date->setStyleSheet("color: #001f21;");
