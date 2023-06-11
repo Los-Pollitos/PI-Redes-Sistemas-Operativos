b3fae0b0bc1e85eeab75811802b538a7a8b7b353
angies23 <angie.solismanzano@ucr.ac.cr>
2023-06-10 21:36:15 -0600

    arreglo en el estado de la solicitud

--- Data_Base/data_base/data_base.cpp
+++ Data_Base/data_base/data_base.cpp
@@ -626,7 +626,7 @@ std::string data_base::consult_requests(std::string user) {
             result += consult_requests.value(0).toString().toStdString() + "-";  // id
             result += consult_requests.value(3).toString().toStdString() + "-";  // type
             result += this->request_type(consult_requests.value(3).toInt(), consult_requests.value(4).toInt()) + ": ";
-            result += this->request_status(consult_requests.value(2).toInt()) += ",";  // status
+            result += this->request_status(consult_requests.value(1).toInt()) += ",";  // status
         } while (consult_requests.next());
         result[result.length()-1] = '\0';  // there was an extra ','
     } else {
@@ -702,9 +702,6 @@ std::string data_base::consult_records(std::string user) {
 
 // TODO(nosotros): documentar
 std::string data_base::consult_record(int id) {
-
-    // (user TEXT, id INTEGER, day INTEGER, month INTEGER, year INTEGER, annotation TEXT)
-
     std::string result = "";
     QSqlQuery consult_record;
 
