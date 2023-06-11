0a15c7e816bed341284065bff2c3f00fd941113f
GoninDS <luisdi2509@gmail.com>
2023-06-10 22:18:49 -0600

    vacation request en db

--- Data_Base/data_base/data_server.h
+++ Data_Base/data_base/data_server.h
@@ -26,6 +26,8 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <QDate>
+
 class data_server {
 private:
     // Atributes
@@ -69,6 +71,7 @@ private:
     void proof_case(std::string remote_ip);
     void obtain_create_information(std::string& username, std::string& identification, std::string& name);
     void see_consult_requests(std::string remote_ip);
+    void vacation_request(std::string remote_ip);
 
 public:
     data_server();
