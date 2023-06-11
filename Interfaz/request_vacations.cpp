2325ad17e12f76261efd50afdc32c09e8d5fe949
GoninDS <luisdi2509@gmail.com>
2023-06-10 19:36:28 -0600

    Formato de fecha sacado exitosamente

--- Interfaz/request_vacations.cpp
+++ Interfaz/request_vacations.cpp
@@ -32,8 +32,8 @@ void request_vacations::set_client(client* local_client) {
 }
 
 void request_vacations::reset_window() {
-    // Empty the to_send string
-    this->to_send = "";
+    // Reset the to_send string to it's base
+    this->to_send = ((char)VACATION_REQUEST);
     this->ui->start_date->setDate(QDate::currentDate());
     this->ui->start_date->setMinimumDate(QDate::currentDate());
 }
@@ -41,12 +41,27 @@ void request_vacations::reset_window() {
 void request_vacations::set_current_user(std::string user) {
     // Used for datagram
     this->user = user;
+    // Add the user to to_send
     this->to_send += user;
     this->to_send += ',';
 }
 
 void request_vacations::on_add_day_clicked() {
+    // Get the selected date
+    QDate date = this->ui->start_date->date();
+    std::string selected_date = date.toString("dd.MM.yyyy").toStdString();
 
+    std::cout << "TENGO " << selected_date <<std::endl;
+
+    // Get the turn from the combobox
+    std::string selected_turn = this->ui->comboBox->currentText().toStdString();
+
+    std::cout << "TENGO " << selected_turn <<std::endl;
+
+    // Format them into to_send
+    this->to_send += selected_date + " - " + selected_turn + "\n";
+
+    std::cout << "TO SEND " << to_send << std::endl;
 }
 
 void request_vacations::on_confirm_clicked() {
